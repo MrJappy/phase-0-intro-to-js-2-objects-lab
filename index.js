90d566115c80fa194bf30e96bd1b9a068e2385bd
+ <+>
2023-01-03 19:59:53 -0600

    commit tree b4b6beff9e846ea556d206d99026460f895a9154

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Jack',
+    streetAdress: '6969 booboo',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    }
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    return employee[key] = value
+};
+
+
